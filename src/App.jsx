b316da555b11fc1de81f79b1fb8f65038a8db0c9
Krish Sinha <krishsinha0304@gmail.com>
2022-08-09 00:12:53 +0530

    added -D sass, used css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Board from './Components/Board';
+import "./styles/root.scss"
 
 const App=() =>{
   return(
-    <>
+    <div className="app">
     <h1>TIC TAC TOE</h1>
     <Board />
-  </>
+  </div>
   );
 };
 
