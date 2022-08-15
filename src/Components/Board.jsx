47c7ab8a07af3638a15d3c0957b35160f3de936c
Krish Sinha <krishsinha0304@gmail.com>
2022-08-15 20:48:49 +0530

    transformed state to game history

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,31 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-
-  const [board,setBoard]=useState(Array(9).fill(null));
-  const [isXNext,setXNext]=useState(false);
-
-  console.log('board rerender');
-
-
-  const handleSquareClick=(position)=>{
-    if(board[position]){
-      return;
-    }
-
-    setBoard((prev)=>{
-      return prev.map((square,pos)=>{
-        if(pos === position){
-          return isXNext?'X':'O';
-        }
-
-        return square;
-      })
-    })
-
-    setXNext((prev)=> !prev);
-  };
+const Board = (board,handleSquareClick) => {
 
   const renderSquare= position =>{
     return(
